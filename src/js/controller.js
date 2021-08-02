69115bf5a54cf741457cdd6c3200f62f9e141503
Ernestocanada <ernesto.industrial@gmail.com>
2021-08-02 16:33:08 +0200

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,10 +119,6 @@ const controlAddRecipes = async function (newRecipe) {
   //location.reload();
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlAddBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -131,6 +127,5 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
-  newFeature();
 };
 init();
